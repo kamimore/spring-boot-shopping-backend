3451b5ddc71daeaa1254c9d70cf25a9bdd7d4d15
negi84 <vipin@mindrops.com>
2025-01-30 11:51:40 +0530

    adding the gitignore and other files

---
+++ src/main/java/com/java/shopbackendproject/ShopBackendProjectApplication.java
@@ -0,0 +1,13 @@
+package com.java.shopbackendproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopBackendProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopBackendProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/java/shopbackendproject/ShopBackendProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.shopbackendproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopBackendProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
