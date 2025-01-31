38a796414f595649f3d4a632a60b047bb99a7019
negi84 <vipin@mindrops.com>
2025-01-31 12:05:46 +0530

    added jsonignore annotation to the product field of category model

--- src/main/java/com/java/shopbackendproject/controller/ImageController.java
+++ src/main/java/com/java/shopbackendproject/controller/ImageController.java
@@ -7,7 +7,6 @@ import org.springframework.core.io.ByteArrayResource;
 import org.springframework.core.io.Resource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
