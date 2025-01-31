11992cb56b91e608eedb48b107eca552990885d5
negi84 <vipin@mindrops.com>
2025-01-31 13:26:15 +0530

    creating the cart and cartItem entity

---
+++ src/main/java/com/java/shopbackendproject/model/Cart.java
@@ -0,0 +1,40 @@
+package com.java.shopbackendproject.model;
+
+import java.math.BigDecimal;
+import java.util.HashSet;
+import java.util.Set;
+
+import org.springframework.boot.autoconfigure.security.SecurityProperties.User;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Cart {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private BigDecimal totalAmount = BigDecimal.ZERO;
+
+    @OneToMany(mappedBy = "cart", cascade = CascadeType.ALL, orphanRemoval = true)
+    private Set<CartItem> items ;
+
+    @OneToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+}
